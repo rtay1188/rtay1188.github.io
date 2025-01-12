0ec041c55a2a6bbb505d9ab357f7273c589d8924
rtay1188 <ryanjbtay@gmail.com>
2025-01-11 16:29:32 -0800

    switch next config to js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// import type { NextConfig } from "next";
+const nextConfig = {
   images: {
     unoptimized: true,
   },
